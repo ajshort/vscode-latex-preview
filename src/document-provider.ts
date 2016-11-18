aa4c571d44539cbecaaaa5868a181b03adc37106
Andrew Short <andrewjshort@gmail.com>
2016-11-18 15:48:53 +1000

    Run latex on save

--- src/document-provider.ts
+++ src/document-provider.ts
@@ -1,17 +1,35 @@
-import * as vscode from "vscode";
+import * as cp from "child_process";
+import { basename, dirname } from "path";
+import { CancellationToken, Event, EventEmitter, TextDocumentContentProvider, Uri } from "vscode";
 
-export default class LatexDocumentProvider implements vscode.TextDocumentContentProvider {
-  private changed = new vscode.EventEmitter<vscode.Uri>();
+export default class LatexDocumentProvider implements TextDocumentContentProvider {
+  private changed = new EventEmitter<Uri>();
 
-  public async provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): Promise<string> {
-    return `Hello, world at ${Date.now()}`;
+  public provideTextDocumentContent(uri: Uri, token: CancellationToken): Thenable<string> {
+    const path = uri.fsPath;
+    const cwd = dirname(path);
+
+    // Build the dvi file. If it fails, display the log.
+    return new Promise(resolve => {
+      cp.exec(`latex -halt-on-error ${quoted(path)}`, { cwd }, (err, out) => {
+        if (err) {
+          return resolve(out);
+        }
+
+        resolve(`Hello, world at ${Date.now()}`);
+      });
+    });
   }
 
-  public update(uri: vscode.Uri) {
+  public update(uri: Uri) {
     this.changed.fire(uri.with({ scheme: "latex-live" }));
   }
 
-  public get onDidChange(): vscode.Event<vscode.Uri> {
+  public get onDidChange(): Event<Uri> {
     return this.changed.event;
   }
+}
+
+function quoted(str: string): string {
+  return '"' + str.replace(/([\\"$])/g, "\\$1") + '"';
 }
