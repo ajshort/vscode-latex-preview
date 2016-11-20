ce62c0db3316b1459c4d17ba1a2cf989906c385e
Andrew Short <andrewjshort@gmail.com>
2016-11-20 21:04:27 +1000

    Show source command

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,52 @@
 import LatexDocumentProvider from "./document-provider";
 import { basename } from "path";
-import * as vscode from "vscode";
+import { ExtensionContext, Uri, ViewColumn, commands, languages, window, workspace } from "vscode";
 
 const LATEX_SELECTOR = { language: "latex", scheme: "file" };
 
-export function activate(ctx: vscode.ExtensionContext) {
+export function activate(ctx: ExtensionContext) {
   // Commands
   ctx.subscriptions.push(
-    vscode.commands.registerCommand("latex-preview.showPreview", showPreview),
-    vscode.commands.registerCommand("latex-preview.showPreviewToSide", showPreviewToSide),
-    vscode.commands.registerCommand("latex-preview.showSource", showSource)
+    commands.registerCommand("latex-preview.showPreview", showPreview),
+    commands.registerCommand("latex-preview.showPreviewToSide", showPreviewToSide),
+    commands.registerCommand("latex-preview.showSource", showSource)
   );
 
   // Document provider
   const renderer = new LatexDocumentProvider(ctx);
 
   ctx.subscriptions.push(renderer);
-  ctx.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider("latex-preview", renderer));
+  ctx.subscriptions.push(workspace.registerTextDocumentContentProvider("latex-preview", renderer));
 
-  ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument(doc => {
-    if (vscode.languages.match(LATEX_SELECTOR, doc) > 0) {
+  ctx.subscriptions.push(workspace.onDidSaveTextDocument(doc => {
+    if (languages.match(LATEX_SELECTOR, doc) > 0) {
       renderer.update(doc.uri);
     }
   }));
 }
 
-function showPreview(uri?: vscode.Uri, column?: vscode.ViewColumn) {
+function showPreview(uri?: Uri, column?: ViewColumn) {
   const previewUri = uri.with({ scheme: "latex-preview" });
   const title = `Preview "${basename(uri.fsPath)}"`;
 
-  return vscode.commands.executeCommand("vscode.previewHtml", previewUri, vscode.ViewColumn.Two, title);
+  return commands.executeCommand("vscode.previewHtml", previewUri, ViewColumn.Two, title);
 }
 
-function showPreviewToSide(uri?: vscode.Uri) {
+function showPreviewToSide(uri?: Uri) {
 }
 
-function showSource() {
+function showSource(uri?: Uri) {
+  if (!uri) {
+    return commands.executeCommand("workbench.action.navigateBack");
+  }
+
+  uri = uri.with({ scheme: "file" });
+
+  for (const editor of window.visibleTextEditors) {
+    if (editor.document.uri.toString() === uri.toString()) {
+      return window.showTextDocument(editor.document, editor.viewColumn);
+    }
+  }
+
+  return workspace.openTextDocument(uri).then(window.showTextDocument);
 }
