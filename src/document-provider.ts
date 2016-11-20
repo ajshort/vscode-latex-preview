e0b5011552c8f29326e712baa2acce91ffced12a
Andrew Short <andrewjshort@gmail.com>
2016-11-20 20:53:33 +1000

    Build preview in a temp dir

--- src/document-provider.ts
+++ src/document-provider.ts
@@ -1,6 +1,7 @@
 import * as cp from "child_process";
 import * as http from "http";
 import { dirname, join } from "path";
+import * as tmp from "tmp";
 import { CancellationToken, ExtensionContext, TextDocumentContentProvider, Uri } from "vscode";
 import * as ws from "ws";
 
@@ -8,6 +9,8 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
   private http: http.Server;
   private websocket: ws.Server;
   private listening: Promise<void>;
+
+  private dirs: { [uri: string]: string } = {};
 
   constructor(private context: ExtensionContext) {
     this.http = http.createServer();
@@ -29,6 +32,11 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
 
   public async provideTextDocumentContent(uri: Uri, token: CancellationToken): Promise<string> {
     await this.listening;
+
+    // Create a temp dir for the preview.
+    this.dirs[uri.toString()] = await new Promise<string>((c, e) => {
+      tmp.dir({ unsafeCleanup: true }, (err, path) => err ? e(err) : c(path));
+    });
 
     const preview = await this.build(uri);
 
@@ -63,7 +71,7 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
    */
   private build(uri: Uri): Promise<Uri> {
     const path = uri.fsPath;
-    const cwd = dirname(path);
+    const cwd = this.dirs[uri.toString()];
 
     return new Promise((resolve, reject) => {
       cp.exec(`pdflatex -jobname=preview -synctex=1 -halt-on-error ${arg(path)}`, { cwd }, (err, out) =>
