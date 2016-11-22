35f08d8e667fca31b1e41feeace1b5a99ff0e877
Andrew Short <andrewjshort@gmail.com>
2016-11-22 15:16:47 +1000

    Fix issues with opening and closing previews

--- src/document-provider.ts
+++ src/document-provider.ts
@@ -5,13 +5,13 @@ import * as tmp from "tmp";
 import { CancellationToken, ExtensionContext, Position, TextDocumentContentProvider, Uri, commands } from "vscode";
 import * as ws from "ws";
 
-interface Preview {
-  path: string;
-  dir: string;
-  connected: Promise<void>;
-  connectedResolve: Function;
-  client?: ws;
-};
+// interface Preview {
+//   path: string;
+//   dir: string;
+//   connected: Promise<void>;
+//   connectedResolve: Function;
+//   client?: ws;
+// };
 
 /**
  * Provides preview content and creates a websocket server which communicates with the preview.
@@ -21,7 +21,10 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
   private server: ws.Server;
   private listening: Promise<void>;
 
-  private previews: Preview[] = [];
+  private directories = new Map<string, string>();
+  private clients = new Map<string, ws>();
+  private connected = new Map<string, Promise<void>>();
+  private connectedResolve = new Map<string, Function>();
 
   constructor(private context: ExtensionContext) {
     this.http = http.createServer();
@@ -41,23 +44,28 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
     this.server.close();
   }
 
+  /**
+   * Returns true if a client with the specified path is connected.
+   */
+  public isPreviewing(path: string): boolean {
+    return this.clients.has(path);
+  }
+
+  /**
+   * Creates a working dir and returns client HTML.
+   */
   public async provideTextDocumentContent(uri: Uri, token: CancellationToken): Promise<string> {
     await this.listening;
 
-    // Create a new preview instance.
-    const preview = <Preview> {
-      path: uri.fsPath,
-      dir: await this.createTempDir(),
-      connected: null,
-      connectedResolve: null,
-    };
+    // Create a working dir and being listening.
+    const path = uri.fsPath;
+    const dir = await this.createTempDir();
 
-    preview.connected = new Promise(c => preview.connectedResolve = c);
-
-    this.previews.push(preview);
+    this.directories[path] = dir;
+    this.listenForConnection(path);
 
     // Build the PDF and generate the preview document.
-    const pdf = await this.build(preview.path, preview.dir);
+    const pdf = await this.build(path, dir);
 
     const { address, port } = this.http.address();
     const ws = `ws://${address}:${port}`;
@@ -72,56 +80,49 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
       <script src="${this.getResourcePath("node_modules/pdfjs-dist/build/pdf.worker.js")}"></script>
       <script src="${this.getResourcePath("out/src/client.js")}"></script>
     </head>
-    <body class="preview" data-path="${attr(preview.path)}" data-websocket="${attr(ws)}" data-pdf="${attr(pdf)}">
+    <body class="preview" data-path="${attr(path)}" data-websocket="${attr(ws)}" data-pdf="${attr(pdf)}">
     </body>
     </html>`;
   }
 
-  public async update(uri: Uri) {
-    const preview = this.getPreview(uri.fsPath);
+  public update(uri: Uri) {
+    const path = uri.fsPath;
 
-    if (typeof preview === "undefined") {
+    if (!this.isPreviewing(path)) {
       return;
     }
 
-    await this.build(preview.path, preview.dir);
-    preview.client.send(JSON.stringify({ type: "update" }));
+    this.build(path, this.directories[path]).then(() => {
+      this.clients.get(path).send(JSON.stringify({ type: "update" }))
+    });
   }
 
   /**
    * Shows a text editor position in the preview.
    */
   public async showPosition(uri: Uri, position: Position) {
-    let preview = this.getPreview(uri.fsPath);
+    const path = uri.fsPath;
 
-    if (typeof preview === "undefined") {
+    if (!this.isPreviewing(path)) {
       await commands.executeCommand("latex-preview.showPreview", uri);
-      preview = this.getPreview(uri.fsPath);
     }
 
     // Make sure the client is connected.
-    await preview.connected;
+    await this.connected.get(path);
 
-    // Get the position.
+    // Get the position and send to the client.
     const rects = await synctex.view({
       line: position.line + 1,
       column: position.character + 1,
-      input: preview.path,
-      output: `${preview.dir}/preview.pdf`,
+      input: path,
+      output: `${this.directories[path]}/preview.pdf`,
     });
 
     if (rects.length === 0) {
       return;
     }
 
-    preview.client.send(JSON.stringify({ type: "show", rect: rects[0] }));
-  }
-
-  /**
-   * Gets the preview object for a tex file path.
-   */
-  private getPreview(path: string): Preview {
-    return this.previews.find(preview => preview.path === path);
+    this.clients.get(path).send(JSON.stringify({ type: "show", rect: rects[0] }));
   }
 
   /**
@@ -135,20 +136,28 @@ export default class LatexDocumentProvider implements TextDocumentContentProvide
     });
   }
 
+  private listenForConnection(path: string) {
+    this.connected.set(path, new Promise(resolve => {
+      this.connectedResolve.set(path, resolve);
+    }));
+  }
+
   private onClientMessage(client: ws, message: any) {
     const data = JSON.parse(message);
 
     if (data.type === "open") {
-      const preview = this.getPreview(data.path);
-      preview.client = client;
-      preview.connectedResolve();
+      const path = data.path;
+
+      this.clients.set(path, client);
+      this.connectedResolve.get(path)();
     }
   }
 
-  private onClientClose(client: ws) {
-    for (let i = 0; i < this.previews.length; i++) {
-      if (this.previews[i].client === client) {
-        this.previews.splice(i, 1);
+  private onClientClose(closed: ws) {
+    for (const [path, client] of this.clients.entries()) {
+      if (closed === client) {
+        this.clients.delete(path);
+        this.listenForConnection(path);
         return;
       }
     }
